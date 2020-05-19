92d18ea2e03084e23a8bc4e93c51438b6071c7f3
Karen Ventura <akv802@gmail.com>
2020-05-18 23:54:23 -0500

    validate content type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  include ExceptionHandler
+  before_action :verify_content_type
+
+  def verify_content_type
+    return if request.headers['Content-Type'] == 'application/json'
+
+    render json: { errors: [ { "type": "invalid", "message": "Unsupported media type" } ] }, status: :unsupported_media_type
+  end
 end
